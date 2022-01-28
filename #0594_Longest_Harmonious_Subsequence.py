3fed515c30b9c8d1c9b2edcce06382d299d4dfb4
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-01-27 22:58:13 -0800

    Create #0594_Longest_Harmonious_Subsequence.py

---
+++ #0594_Longest_Harmonious_Subsequence.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findLHS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        counter = collections.Counter(nums)
+        max_count = 0
+        for key in counter:
+            if key + 1 in counter:
+                max_count = max(max_count, counter[key] + counter[key + 1])
+        return max_count
