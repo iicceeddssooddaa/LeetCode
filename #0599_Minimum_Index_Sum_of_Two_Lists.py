e885046513f4b544b0b32aa8fbf0a1b377e6d042
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-01-28 07:14:05 -0800

    Update #0599_Minimum_Index_Sum_of_Two_Lists.py

--- #0599_Minimum_Index_Sum_of_Two_Lists.py
+++ #0599_Minimum_Index_Sum_of_Two_Lists.py
@@ -8,13 +8,12 @@ class Solution(object):
         _dict = {}
         index = {}
         for pos, item in enumerate(list1):
-            _dict[str(item)] = -pos
+             _dict[item] = -pos
         for pos, item in enumerate(list2):
             if item in _dict:
                 pos_sum = _dict[item]*(-1) + pos
                 index[pos_sum] = item
         min_sum = min(key for key in index)
-        return "".join(index[min_sum])
-        # string = "".join(index[min_sum])
-        # return string
+        return [index[min_sum]]
+        
         
