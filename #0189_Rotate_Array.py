a43728f5065014e75b48b479af0cf9b70134a7ae
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-01-29 18:07:35 -0800

    Update #0189_Rotate_Array.py
    可能还是没有O(1)

--- #0189_Rotate_Array.py
+++ #0189_Rotate_Array.py
@@ -12,3 +12,18 @@ class Solution(object):
         del nums[:-k]
         nums.extend(cache_list)
         没有O(1)
+----------
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        if k == 0:
+            return nums
+        nums[:k], nums[k:] = nums[-k:], nums[:-k] 
+-------------
+if k%len(nums) == 0: return nums
+        nums[:k % len(nums)], nums[k % len(nums):] = nums[-(k % len(nums)):], nums[:-(k % len(nums))] 
