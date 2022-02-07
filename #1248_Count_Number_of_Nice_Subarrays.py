4bc9501bb9cdbfc3b2dc03d1cc010a811382c410
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-06 17:19:15 -0800

    Update #1248_Count_Number_of_Nice_Subarrays.py

--- #1248_Count_Number_of_Nice_Subarrays.py
+++ #1248_Count_Number_of_Nice_Subarrays.py
@@ -5,6 +5,9 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # 我们只关心实质结构[偶数的个数，一个奇数，偶数的个数，一个奇数，。。。，偶数的个数]
+        # 最终结果就是连续k个奇数前后偶数的个数（分别加一）乘积之和
+        # 计数问题
         count, n, parity_list = 0, len(nums), []
         for i in range(n):
             count += 1
