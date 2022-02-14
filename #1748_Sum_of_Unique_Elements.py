9dbd1b0b40eede239a99f3a1e25fb137cad641fe
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-14 00:32:37 -0800

    Update #1748_Sum_of_Unique_Elements.py

--- #1748_Sum_of_Unique_Elements.py
+++ #1748_Sum_of_Unique_Elements.py
@@ -13,3 +13,15 @@ class Solution(object):
                 result -= num
                 _dict[num] += 1
         return result
+----------
+class Solution(object):
+    def sumOfUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result, _dict = 0, {}
+        _dict = collections.Counter(nums)
+        for key, value in _dict.items():
+            if value == 1: result += key
+        return result
