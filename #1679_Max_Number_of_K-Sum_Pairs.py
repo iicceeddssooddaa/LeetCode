f8aa16d076d9b06eb7aa64ddb49a82566c9b32cf
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-16 13:19:24 -0800

    Update #1679_Max_Number_of_K-Sum_Pairs.py

--- #1679_Max_Number_of_K-Sum_Pairs.py
+++ #1679_Max_Number_of_K-Sum_Pairs.py
@@ -17,3 +17,25 @@ class Solution(object):
                 count += contribute
                 counter[key] -= contribute * 2
         return count
+--------
+# 这还不如排序
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        i, j, count = 0, len(nums) - 1, 0
+        while i < j:
+            if nums[i] + nums[j] == k:
+                i += 1
+                j -= 1
+                count += 1
+            elif nums[i] + nums[j] < k:
+                i += 1
+            else: j -= 1
+        return count
+                
+        
