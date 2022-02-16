c3745c347c67ee8583656796a2d27eb759ab7641
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-16 10:13:28 -0800

    Update #0021_Merge_Two_Sorted_Lists.py

--- #0021_Merge_Two_Sorted_Lists.py
+++ #0021_Merge_Two_Sorted_Lists.py
@@ -36,3 +36,34 @@ class Solution(object):
         if cur2:
             tail.next = cur2
         return head
+-----------------------
+# 时隔数日再写，结构完整了不少
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not list1: return list2
+        if not list2: return list1
+        temp1, temp2, head1, head2 = list1, list2, list1, list2
+        temp = ListNode(0)
+        head = temp
+        while head1 and head2:
+            if head1.val < head2.val:
+                temp.next = head1
+                temp = temp.next
+                head1 = head1.next
+            else:
+                temp.next = head2
+                temp = temp.next
+                head2 = head2.next
+        if head1: temp.next = head1
+        if head2: temp.next = head2
+        return head.next
