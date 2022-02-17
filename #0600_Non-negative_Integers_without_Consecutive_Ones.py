ed2af8bde3bf08f166e89d1ac68f951d531c665a
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-16 18:57:18 -0800

    Update #0600_Non-negative_Integers_without_Consecutive_Ones.py

--- #0600_Non-negative_Integers_without_Consecutive_Ones.py
+++ #0600_Non-negative_Integers_without_Consecutive_Ones.py
@@ -10,3 +10,18 @@ class Solution(object):
         for i in range(4, k + 3): fib[i%3] = fib[(i - 1)%3] + fib[(i - 2)%3]
         if n * 2/3 >= 2**k: return fib[(k + 2)%3] + fib[(k + 1)%3]
         else: return fib[(k + 2)%3] + self.findIntegers(n - 2**k)
+-----------------------
+# 有显著快一点。暴力算这时候就比反复递归强多了。
+class Solution(object):
+    def findIntegers(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 3: return [1,2,3][n]
+        x = "{0:b}".format(n)
+        k = len(x) - 1
+        num1 = (((1 + sqrt(5))/2)**(k + 3) - ((1 - sqrt(5))/2)**(k + 3))/sqrt(5)
+        num2 = (((1 + sqrt(5))/2)**(k + 2) - ((1 - sqrt(5))/2)**(k + 2))/sqrt(5)
+        if n * 2/3 >= 2**k: return int(num1)
+        else: return int(num2 + self.findIntegers(n - 2**k))
