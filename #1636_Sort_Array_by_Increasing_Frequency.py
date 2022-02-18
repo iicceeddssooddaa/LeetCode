4b411ce81b07cf97d2971633377a9dcaa9bcf35b
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-18 01:35:56 -0800

    Update #1636_Sort_Array_by_Increasing_Frequency.py

--- #1636_Sort_Array_by_Increasing_Frequency.py
+++ #1636_Sort_Array_by_Increasing_Frequency.py
@@ -18,3 +18,18 @@ class Solution(object):
                 _dict[i].sort(reverse = True)
                 for num in _dict[i]: result.extend([num] * i)
         return result
+----------
+# 既然能用上高维列表的排序，当然要用。如果同频则降序，那么构造[频次,-1 * 数字]的序列进行排序即可。然后还原。
+# 快多了。
+class Solution(object):
+    def frequencySort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        counter = collections.Counter(nums)
+        sto, result = [], []
+        for key, value in counter.items(): sto.append([value, -1 * key])
+        sto.sort()
+        for record in sto: result.extend([record[1]*-1] * record[0])
+        return result
