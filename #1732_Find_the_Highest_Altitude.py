302a428453faa7e258e07f01e20f83a38b7ffd4e
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-18 00:59:07 -0800

    Update #1732_Find_the_Highest_Altitude.py

--- #1732_Find_the_Highest_Altitude.py
+++ #1732_Find_the_Highest_Altitude.py
@@ -10,3 +10,17 @@ class Solution(object):
             max_alt = max(alt, max_alt)
         return max_alt
       # 或许遇到下降的可以不跑max部分
+-----
+class Solution(object):
+    def largestAltitude(self, gain):
+        """
+        :type gain: List[int]
+        :rtype: int
+        """
+        alt, max_alt = 0, 0
+        for disc in gain:
+            alt += disc
+            if disc >= 0:
+                max_alt = max(alt, max_alt)
+        return max_alt
+    # 瞬间提速，节省空间
