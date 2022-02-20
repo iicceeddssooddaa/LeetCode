d3b9daa1c923805dd2df2855e229486512c2722b
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-20 02:24:32 -0800

    Update #1688_Count_of_Matches_in_Tournament.py

--- #1688_Count_of_Matches_in_Tournament.py
+++ #1688_Count_of_Matches_in_Tournament.py
@@ -6,3 +6,13 @@ class Solution(object):
         """
         return n - 1
       # 为啥这个速度有问题
+---------
+# 总之尽量少在return步骤做运算。下面这个👇🏻居然明显快。这都啥啊
+class Solution(object):
+    def numberOfMatches(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        cache = n - 1
+        return cache
