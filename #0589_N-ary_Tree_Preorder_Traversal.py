6be53fe67f32e54cddd9df98678f81ea4465c2a3
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-21 02:39:46 -0800

    Create #0589_N-ary_Tree_Preorder_Traversal.py

---
+++ #0589_N-ary_Tree_Preorder_Traversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root: return []
+        nodelist = [root.val]
+        for child in root.children: nodelist.extend(self.preorder(child))
+        return nodelist
