af3a19ae0716e27b11d73cf71fe698cf3a869c7e
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-02-25 11:44:26 -0800

    Update #0101_Symmetric_Tree.py

--- #0101_Symmetric_Tree.py
+++ #0101_Symmetric_Tree.py
@@ -30,3 +30,33 @@ class Solution(object):
             return cache
         pre, post = preOrderTraversal(root), postOrderTraversal(root)
         return pre == post[-1::-1]
+----------------------
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def areSymmetric(node1, node2):
+            if node1.val != node2.val: return False
+            if node1.left and not node2.right: return False
+            if not node1.left and node2.right: return False
+            if not node1.left and not node2.right: pass
+            else: 
+                if not areSymmetric(node1.left, node2.right): return False
+            if node1.right and not node2.left: return False
+            if not node1.right and node2.left: return False
+            if not node1.right and not node2.left: pass
+            else: 
+                if not areSymmetric(node1.right, node2.left): return False
+            return True
+        if not root.left and not root.right: return True
+        if not root.left and root.right: return False
+        if root.left and not root.right: return False
+        return areSymmetric(root.left, root.right)
