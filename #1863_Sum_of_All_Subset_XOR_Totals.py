7e9dbae6940c7e30468122e4c2488f36950b292b
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-03-01 10:27:47 -0800

    Update #1863_Sum_of_All_Subset_XOR_Totals.py

--- #1863_Sum_of_All_Subset_XOR_Totals.py
+++ #1863_Sum_of_All_Subset_XOR_Totals.py
@@ -9,3 +9,18 @@ class Solution(object):
             n = len(result)
             for i in range(n): result.append(num ^ result[i])
         return sum(result)
+----------------
+class Solution(object):
+    def subsetXORSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result, total = [0], 0
+        for num in nums:
+            n = len(result)
+            for i in range(n): 
+                cache = num ^ result[i]
+                result.append(num ^ result[i])
+                total += cache
+        return total
