35e4c4fb22e7e53856bcc6a3a5c9c755e28fd405
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-03-02 14:02:59 -0800

    Update #2057_Smallest_Index_With_Equal_Value.py

--- #2057_Smallest_Index_With_Equal_Value.py
+++ #2057_Smallest_Index_With_Equal_Value.py
@@ -9,3 +9,12 @@ class Solution(object):
             if i%10 != nums[i]: i += 1
             else: break
         return i if i < len(nums) else -1
+--------------
+class Solution(object):
+    def smallestEqual(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        sto = [(nums[i] == i%10) for i in range(len(nums))]
+        return -1 if 1 not in sto else sto.index(1) 
