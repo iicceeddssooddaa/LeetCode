dae38b6a8b3c9ff03a50442d03a68d513a0ac5be
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-03-02 13:47:33 -0800

    Create #1450_Number_of_Students_Doing_Homework_at_a_Given_Time.py

---
+++ #1450_Number_of_Students_Doing_Homework_at_a_Given_Time.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def busyStudent(self, startTime, endTime, queryTime):
+        """
+        :type startTime: List[int]
+        :type endTime: List[int]
+        :type queryTime: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(startTime)): count += startTime[i] <= queryTime <= endTime[i]
+        return count
