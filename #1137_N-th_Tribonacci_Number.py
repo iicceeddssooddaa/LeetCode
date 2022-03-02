22b3f35cf84d770a7cb77960e2d9e4bd597282ef
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-03-02 14:28:46 -0800

    Update and rename #1137_N-th_Tribonacci_Number to #1137_N-th_Tribonacci_Number.py

--- #1137_N-th_Tribonacci_Number.py
+++ #1137_N-th_Tribonacci_Number.py
@@ -9,3 +9,13 @@ class Solution(object):
             T.append(T[i - 3] + T[i - 2] + T[i - 1])
         return T[n]
         #可以考虑直接模运算迭代，只用四位
+-------
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        T = [0,1,1]
+        for i in range(3,n + 1): T[i%3] = T[(i - 3)%3] + T[(i - 2)%3] + T[(i - 1)%3]
+        return T[n%3]
