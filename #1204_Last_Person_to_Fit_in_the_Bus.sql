5339d7b2a24c19780ec1469a35dd3ce4b4258c71
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-05-26 22:40:19 -0700

    Create #1204_Last_Person_to_Fit_in_the_Bus.sql

---
+++ #1204_Last_Person_to_Fit_in_the_Bus.sql
@@ -0,0 +1,9 @@
+WITH t AS (
+    SELECT person_name, weight, SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+)
+SELECT person_name
+FROM t
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
+LIMIT 1;
