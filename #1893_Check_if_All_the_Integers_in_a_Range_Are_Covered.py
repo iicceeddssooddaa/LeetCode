13ceb17424eca7cbc2bad20b7a3ea9507756e93a
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-06-07 13:01:21 -0700

    Update #1893_Check_if_All_the_Integers_in_a_Range_Are_Covered.py

--- #1893_Check_if_All_the_Integers_in_a_Range_Are_Covered.py
+++ #1893_Check_if_All_the_Integers_in_a_Range_Are_Covered.py
@@ -13,3 +13,21 @@ class Solution(object):
         for i in range(left, right + 1):
             if i not in set_cov: return False
         return True
+---------
+class Solution(object):
+    def isCovered(self, ranges, left, right):
+        """
+        :type ranges: List[List[int]]
+        :type left: int
+        :type right: int
+        :rtype: bool
+        """
+        n = len(ranges)
+        for i in range(left, right + 1):
+            j = 0
+            not_covered = True
+            while j < n and not_covered:
+                if ranges[j][0] <= i <= ranges[j][1]: not_covered = False
+                j += 1
+            if not_covered: return False
+        return True
