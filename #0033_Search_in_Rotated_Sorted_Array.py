3df69c887ec7e5da82ab24d6d7b284ea8c6785a6
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-03 16:23:35 -0700

    Update #0033_Search_in_Rotated_Sorted_Array.py

--- #0033_Search_in_Rotated_Sorted_Array.py
+++ #0033_Search_in_Rotated_Sorted_Array.py
@@ -6,36 +6,12 @@ class Solution(object):
         :rtype: int
         """
         if len(nums) == 1: return 0 if target == nums[0] else -1
-        # search for position of min of the list
-        if nums[-1] < nums[-2]:
-            left, right = len(nums) - 1, len(nums) - 1
-        elif nums[0] > nums[1]:
-            left, right = 1, 1
-        elif nums[0] < nums[-1]:
-            left, right = 0, 0
-        else:
-            left, right = 0, len(nums) - 1
+        left, right = 0, len(nums) - 1
         while left < right:
+            if target == nums[left]: return left
+            if target == nums[right]: return right
             mid = (left + right)//2
-            if nums[0] > nums[mid]:
-                right = mid
-            elif nums[0] < nums[mid]:
-                left = mid + 1
-            else:
-                left, right = mid, mid
-        # search the half it belongs to
-        if target == nums[0]: return 0
-        elif target < nums[0]: right = len(nums) - 1
-        elif left == 0:
-             right = len(nums) - 1
-        else: left, right = 0, right - 1
-        if target == nums[left]: return left
-        if target == nums[right]: return right
-        while left < right:
-            mid = (left + right)//2
-            if target < nums[mid]:
-                right = mid - 1
-            elif target > nums[mid]:
-                left = mid + 1
-            else: left, right = mid, mid
+            if target == nums[mid]: return mid
+            if (nums[left] < target < nums[mid]) or (nums[mid] < nums[right] < nums[left] < target) or (target < nums[mid] < nums[right] < nums[left]): right = mid - 1
+            else: left = mid + 1
         return left if target == nums[left] else -1
