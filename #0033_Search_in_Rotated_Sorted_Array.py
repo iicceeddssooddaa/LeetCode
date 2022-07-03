7564450fb397da74498cfa7d6298768d74bcb037
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-03 16:35:09 -0700

    Update #0033_Search_in_Rotated_Sorted_Array.py

--- #0033_Search_in_Rotated_Sorted_Array.py
+++ #0033_Search_in_Rotated_Sorted_Array.py
@@ -15,3 +15,13 @@ class Solution(object):
             if (nums[left] < target < nums[mid]) or (nums[mid] < nums[right] < nums[left] < target) or (target < nums[mid] < nums[right] < nums[left]): right = mid - 1
             else: left = mid + 1
         return left if target == nums[left] else -1
+"""
+USE 2, 4, 6 to check all posibilities:
+    246, 264, 426, 462, 624, 642
+    Among these 264, 426, 642 violate monotonicity so cross out.
+    Let's see only cases where right side updated:
+    1. 246. 2 < 4 so if in left half, it must be between.
+    2. 462. Same.
+    3. 624. Now it could be a 7 or a 1. So then mid < right < left immediately. And the extra is that target is the smallest or largets.
+    Therefore we obtained the huge if condition.
+"""
